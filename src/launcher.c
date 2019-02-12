e5e87b3fd5005a0a4143a4f864455aa713c410be
Antoine <agiordan@z1r4p5.le-101.fr>
2019-02-12 19:35:50 +0100

    modif opti

--- src/launcher.c
+++ src/launcher.c
@@ -6,7 +6,7 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/05 15:56:00 by agiordan     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/12 19:03:59 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/12 19:35:25 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,23 +17,15 @@ int	main(int ac, char **av)
 {
 	int		i;
 	char	*exec;
-	char	*input;
-	char	*et;
 
-	et = ft_strdup(" &");
 	i = 0;
 	if (ac > 1)
 		while (++i < ac)
 		{
-			exec = ft_strdup("./fractol ");
-			input = ft_strdup(av[i]);
-			ft_strcat(exec, input);
-			ft_strcat(exec, et);
+			exec = ft_strjoin("./fractol ", av[i]);
+			ft_strcat(exec, " &");
 			system(exec);
-			ft_strdel(&input);
 			ft_strdel(&exec);
 		}
-
-	ft_strdel(&et);
 	return (0);
 }
