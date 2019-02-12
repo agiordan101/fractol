1ed0e71e20e9a1dc6bf89d8fcbfe32babf312a4a
Antoine <agiordan@z1r4p5.le-101.fr>
2019-02-12 19:05:00 +0100

    Ajout du & pour programme en simultane

--- src/launcher.c
+++ src/launcher.c
@@ -6,7 +6,7 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/05 15:56:00 by agiordan     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/07 15:28:53 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/12 19:03:59 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,16 +18,22 @@ int	main(int ac, char **av)
 	int		i;
 	char	*exec;
 	char	*input;
+	char	*et;
 
+	et = ft_strdup(" &");
 	i = 0;
 	if (ac > 1)
 		while (++i < ac)
 		{
 			exec = ft_strdup("./fractol ");
 			input = ft_strdup(av[i]);
-			system(ft_strcat(exec, input));
-			ft_strdel(&exec);
+			ft_strcat(exec, input);
+			ft_strcat(exec, et);
+			system(exec);
 			ft_strdel(&input);
+			ft_strdel(&exec);
 		}
+
+	ft_strdel(&et);
 	return (0);
 }
